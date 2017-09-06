7e149e22fdc0562189a0ba319fa70f077c473464
r4wm <raymondmintz11@gmail.com>
2017-09-05 19:01:23 -0700

    update theme and ignore ido.last and added cool magit

--- init.el
+++ init.el
@@ -16,10 +16,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (wheatgrass)))
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(custom-enabled-themes (quote (deeper-blue)))
  '(package-selected-packages
    (quote
-    (flymake-go go-autocomplete go-guru auto-complete exec-path-from-shell go-mode paredit geiser blank-mode yaml-mode flycheck-pyflakes flycheck swiper smex jedi))))
+    (flymake-go go-autocomplete go-guru auto-complete exec-path-from-shell go-mode paredit geiser blank-mode yaml-mode flycheck-pyflakes flycheck swiper smex jedi magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -28,6 +32,7 @@
  )
 
 (package-initialize)
+(menu-bar-mode 0)
 
 ;;install missing packages if any
 (dolist (package package-selected-packages)
