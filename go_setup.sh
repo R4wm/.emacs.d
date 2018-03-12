9395834ef09c90f1638e41d798aad0116f5ccd53
root <root@kafka001.dlx.lc4>
2018-03-12 06:54:00 +0000

    show progress..

--- go_setup.sh
+++ go_setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+echo "go get -u golang.org/x/tools/cmd/..."
 go get -u golang.org/x/tools/cmd/...
+echo "go get -u github.com/rogpeppe/godef/..."
 go get -u github.com/rogpeppe/godef/...
+echo "go get -u github.com/nsf/gocode"
 go get -u github.com/nsf/gocode
+echo "go get -u golang.org/x/tools/cmd/goimports"
 go get -u golang.org/x/tools/cmd/goimports
+echo "go get -u golang.org/x/tools/cmd/guru"
 go get -u golang.org/x/tools/cmd/guru
+echo "go get -u github.com/dougm/goflymake"
 go get -u github.com/dougm/goflymake
+echo "All done, enjoy!"
