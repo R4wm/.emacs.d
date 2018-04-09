458d14d590d0a77b8b08b56126acf64222f57568
r4wm <raymondmintz11@gmail.com>
2018-04-09 19:41:09 +0000

    cause blocking copy pasta

--- init.el
+++ init.el
@@ -138,14 +138,14 @@
 (defun my-go-mode-hook ()
   (add-hook 'before-save-hook 'gofmt-before-save) ; gofmt before every save
   ; Godef jump key binding
-  (local-set-key (kbd "M-.") 'godef-jump)
-  (local-set-key (kbd "M-*") 'pop-tag-mark)
+  ;; (local-set-key (kbd "M-.") 'godef-jump)
+  ;; (local-set-key (kbd "M-*") 'pop-tag-mark)
   (auto-complete-mode)
   (electric-pair-mode)
-  (local-set-key (kbd "M-p") 'compile)            ; Invoke compiler
-  (local-set-key (kbd "M-P") 'recompile)          ; Redo most recent compile cmd
-  (local-set-key (kbd "M-]") 'next-error)         ; Go to next error (or msg)
-  (local-set-key (kbd "M-[") 'previous-error)     ; Go to previous error or msg
+  ;; (local-set-key (kbd "M-p") 'compile)            ; Invoke compiler
+  ;; (local-set-key (kbd "M-P") 'recompile)          ; Redo most recent compile cmd
+  ;; (local-set-key (kbd "M-]") 'next-error)         ; Go to next error (or msg)
+  ;; (local-set-key (kbd "M-[") 'previous-error)     ; Go to previous error or msg
   (setq gofmt-command "goimports")
   )
 
