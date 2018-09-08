3d658e27444ee4f3b2849289a77859395e1e0282
rmintz <raymond.mintz@verizondigitalmedia.com>
2018-09-08 13:57:55 -0700

    c++ stuff

--- init.el
+++ init.el
@@ -1,62 +1,45 @@
-;;; package --- Summary
-;;; Commentary:
-;;Rays init.el
-
-;;; Code:
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (add-to-list
-   'package-archives
-   ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
-   '("melpa" . "http://melpa.milkbox.net/packages/")
-   t))
+(require 'package)
+(add-to-list 'package-archives
+         '("melpa" . "http://melpa.org/packages/") t)
 
 (package-initialize)
 
-(setq package-selected-packages
-      '(
-	go-autocomplete
-	flymake-go
-	go-guru
-	auto-complete
-	exec-path-from-shell
-	go-mode
-	paredit
-	geiser
-	yaml-mode
-	flycheck-pyflakes
-	flycheck
-	swiper
-	smex
-	jedi
-    ))
-
-(defun install-packages ()
-  "Install all required packages."
-  (interactive)
-  (unless package-archive-contents
+(when (not package-archive-contents)
     (package-refresh-contents))
-  (dolist (package package-selected-packages)
-    (unless (package-installed-p package)
-      (package-install package))))
 
-(install-packages)
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(add-to-list 'load-path "~/.emacs.d/custom")
+
+(require 'setup-general)
+(if (version< emacs-version "24.4")
+    (require 'setup-ivy-counsel)
+  (require 'setup-helm)
+  (require 'setup-helm-gtags))
+;; (require 'setup-ggtags)
+(require 'setup-cedet)
+(require 'setup-editing)
 
 
+
+;; function-args
+;; (require 'function-args)
+;; (fa-config-default)
+;; (define-key c-mode-map  [(tab)] 'company-complete)
+;; (define-key c++-mode-map  [(tab)] 'company-complete)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
- '(custom-enabled-themes (quote (wheatgrass)))
+ '(flymake-google-cpplint-command "/home/rmintz/.local/bin/cpplint")
  '(package-selected-packages
    (quote
-    (nginx-mode lua-mode go-autocomplete flymake-go go-guru auto-complete exec-path-from-shell go-mode paredit geiser yaml-mode flycheck-pyflakes flycheck swiper smex jedi))))
-
+    (swiper flymake-cursor flymake-google-cpplint flymake-cppcheck sr-speedbar ggtags zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -64,100 +47,71 @@
  ;; If there is more than one, they won't work right.
  )
 
+(setq
+ helm-gtags-ignore-case t
+ helm-gtags-auto-update t
+ helm-gtags-use-input-at-cursor t
+ helm-gtags-pulse-at-cursor t
+ helm-gtags-prefix-key "\C-cg"
+ helm-gtags-suggested-key-mapping t
+ )
 
-(menu-bar-mode 0)
-;; (scroll-bar-mode 0)
-;; (tool-bar-mode 0)
-;;install missing packages if any
-(dolist (package package-selected-packages)
-  (unless (package-installed-p package)
-    (package-install package)))
+(require 'helm-gtags)
+;; Enable helm-gtags-mode
+(add-hook 'dired-mode-hook 'helm-gtags-mode)
+(add-hook 'eshell-mode-hook 'helm-gtags-mode)
+(add-hook 'c-mode-hook 'helm-gtags-mode)
+(add-hook 'c++-mode-hook 'helm-gtags-mode)
+(add-hook 'asm-mode-hook 'helm-gtags-mode)
 
-;;set some global things you want
-(electric-pair-mode t)
-(show-paren-mode t)
-(global-linum-mode 1) ;always start with linum
+(define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+(define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
+(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
 
-;; some appearance
-(ido-mode t)
-(tool-bar-mode 0)
-;;(scroll-bar-mode 0)
-(setq inhibit-startup-screen t)
-(when window-system (set-frame-size (selected-frame) 120 46))
+;; Find definitions in current buffer
+(setq-local imenu-create-index-function #'ggtags-build-imenu-index)
+(setq speedbar-show-unknown-files t)
 
-;; keys
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "C-s") 'swiper)
-(global-set-key (kbd "C-x g") 'magit-status)
-(global-set-key (kbd "<C-s-268632070>") 'toggle-frame-fullscreen)
-
-;; deft
-(setq deft-extensions '("md" "txt"))
-(global-set-key [f8] 'deft)
-(setq deft-use-filename-as-title t)
-(setq deft-use-filter-string-for-filename t)
-
-;; font
-(set-face-attribute 'default nil :family "PT Mono" :height 140 :weight 'normal)
+;; Autocomplete for c++
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
 
 
 ;; scrolling
 (setq mouse-wheel-scroll-amount '(2 ((shift) . 2)))
 (setq mouse-wheel-progressive-speed nil)
 (setq scroll-step            1
-      scroll-conservatively  10000)
+      scroll-conservatively 10000)
 
-;;python stuff
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:complete-on-dot t)                 ; optional
-(global-flycheck-mode)
+;; Package: smartparens
+(require 'smartparens-config)
+(show-smartparens-global-mode +1)
+(smartparens-global-mode 1)
 
-;;paredit stuff
-(add-hook 'scheme-mode-hook 'paredit-mode)
-(add-hook 'scheme-mode-hook 'run-guile)
-
-;; ;;NO WHITESPACE
-;;(setq indent-tabs-mode nil)
-;; (add-hook 'before-save-hook 'whitespace-cleanup)
-
-;; Spaces 4 lyfe
-(setq-default indent-tabs-mode nil)
-
-;;GOLANG STUFF
-;;ENV STUFF: invoke shell and print variables.. for exec-from-shell
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize)
-  (exec-path-from-shell-copy-env "GOPATH"))
-
-;; auto fmt code on save
-(add-to-list 'exec-path "~/Development/go/bin")
+;; when you press RET, the curly braces automatically
+;; add another newline
+(sp-with-modes '(c-mode c++-mode)
+               (sp-local-pair "{" nil :post-handlers '(("||\n[i]" "RET")))
+               (sp-local-pair "/*" "*/" :post-handlers '((" | " "SPC")
+                                                         ("* ||\n[i]" "RET"))))
 
 
-(add-to-list 'load-path "~/.emacs.d/lisp/")
-(require 'go-guru)
+(global-set-key (kbd "RET") 'newline-and-indent)  ; automatically indent when press RET
 
-;;function for GO Autocomplete
-(defun auto-complete-for-go ()
-  (auto-complete-mode 1))
+(global-semantic-idle-summary-mode 1)
 
-;;do nice things with go-guru
-(go-guru-hl-identifier-mode)
-(defun my-go-mode-hook ()
-  (add-hook 'before-save-hook 'gofmt-before-save) ; gofmt before every save
-  ; Godef jump key binding
-  ;; (local-set-key (kbd "M-.") 'godef-jump)
-  ;; (local-set-key (kbd "M-*") 'pop-tag-mark)
-  (auto-complete-mode)
-  (electric-pair-mode)
-  ;; (local-set-key (kbd "M-p") 'compile)            ; Invoke compiler
-  ;; (local-set-key (kbd "M-P") 'recompile)          ; Redo most recent compile cmd
-  ;; (local-set-key (kbd "M-]") 'next-error)         ; Go to next error (or msg)
-  ;; (local-set-key (kbd "M-[") 'previous-error)     ; Go to previous error or msg
-  (setq gofmt-command "goimports")
-  )
 
-(add-hook 'go-mode-hook 'my-go-mode-hook)
-(add-hook 'go-mode-hook 'auto-complete-for-go)
-(add-hook 'before-save-hook 'gofmt-before-save)
-;;; init.el ends here
+;; ;; FLymake c++ google style
+;; (defun my:flymake-google-init()
+;;   (require 'flymake-google-cpplint)
+;;   (custom-set-variables
+;;    '(flymake-google-cpplint-command "/home/rmintz/.local/bin/cpplint"
+;;   (flymake-google-cpplint-load))))
 
+;; (add-hook 'c-mode-hook 'my:flymake-google-init)
+;; (add-hook 'c++-mode-hook 'my:flymake-google-init)
+
+(global-set-key (kbd "C-s") 'swiper)
