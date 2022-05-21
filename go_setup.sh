680d2b23c7a18b49a931ebba7fc226030b0edaf4
r4wm <raymondmintz11@gmail.com>
2022-05-21 05:55:37 +0100

    fix go_setup for golang == 1.18

--- go_setup.sh
+++ go_setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-echo "go get -u golang.org/x/tools/cmd/..."
-go get -u golang.org/x/tools/cmd/...
-echo "go get -u github.com/rogpeppe/godef/..."
-go get -u github.com/rogpeppe/godef/...
-echo "go get -u github.com/nsf/gocode"
-go get -u github.com/nsf/gocode
-echo "go get -u golang.org/x/tools/cmd/goimports"
-go get -u golang.org/x/tools/cmd/goimports
-echo "go get -u golang.org/x/tools/cmd/guru"
-go get -u golang.org/x/tools/cmd/guru
-echo "go get -u github.com/dougm/goflymake"
-go get -u github.com/dougm/goflymake
+set -e
+echo "go install golang.org/x/tools/cmd/goimports@latest"
+go install golang.org/x/tools/cmd/goimports@latest
+echo "go install github.com/rogpeppe/godef@latest"
+go install github.com/rogpeppe/godef@latest
+echo "go install github.com/nsf/gocode"
+go install github.com/nsf/gocode@latest
+echo "go install golang.org/x/tools/cmd/guru"
+go install golang.org/x/tools/cmd/guru@latest
+echo "go install github.com/dougm/goflymake"
+go install github.com/dougm/goflymake@latest
 echo "All done, enjoy!"
